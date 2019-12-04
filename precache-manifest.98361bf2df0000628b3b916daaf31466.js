a0a09e342d8fc8a8893bc011eb2f6299cc5c6f2a
eshankasun <54253735+eshankasun@users.noreply.github.com>
2019-12-04 22:47:17 +0530

    second commit
    app complete.

---
+++ precache-manifest.98361bf2df0000628b3b916daaf31466.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cb42f92b9fc9e304d2796fe2f0698da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "402a907179970a36b470",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "75959a441b507f2368c8",
+    "url": "/static/css/main.56dae0dd.chunk.css"
+  },
+  {
+    "revision": "402a907179970a36b470",
+    "url": "/static/js/2.ee428d40.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ee428d40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "75959a441b507f2368c8",
+    "url": "/static/js/main.dcfe6a17.chunk.js"
+  },
+  {
+    "revision": "01e5a42a0a779b392583",
+    "url": "/static/js/runtime-main.58c14e21.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
